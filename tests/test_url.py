abf971c818489a34d652af367dcf05a5dad3f5b3
Abdu <abdujabbar.mirkhalikov@gmail.com>
2022-05-31 00:05:03 +0500

    Fix tests

--- tests/test_url.py
+++ tests/test_url.py
@@ -3,24 +3,21 @@ from page_loader.url import to_file
 
 
 @pytest.mark.parametrize(
-    "test_case, default_ext, expected",
+    "test_case, expected",
     [
         (
             "https://ru.hexlet.io/teams",
-            '.html',
             "ru-hexlet-io-teams.html"
         ),
         (
             "https://ru.hexlet.io/courses",
-            '',
-            "ru-hexlet-io-courses"
+            "ru-hexlet-io-courses.html"
         ),
         (
             "https://pythonist.ru/chto-vy-znaete-o-list-dict-comprehensions/",
-            '.html',
             "pythonist-ru-chto-vy-znaete-o-list-dict-comprehensions.html"
         )
     ]
 )
-def test_sluggify(test_case, default_ext, expected):
-    assert to_file(test_case, default_ext) == expected
+def test_sluggify(test_case, expected):
+    assert to_file(test_case) == expected
