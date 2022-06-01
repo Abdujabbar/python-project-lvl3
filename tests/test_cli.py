2f8107283b8b86aed29c3a371b332e2035b4e370
Abdu <abdujabbar.mirkhalikov@gmail.com>
2022-06-01 17:39:29 +0500

    Modify

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,7 +1,5 @@
-import requests_mock
 import pytest
 import subprocess
-from tests import FIXTURES_PATH
 
 
 def capture(command):
@@ -11,33 +9,6 @@ def capture(command):
 
     out, err = proc.communicate()
     return out, err, proc.returncode
-
-
-@pytest.mark.parametrize(
-    "url, output, expected_code, path",
-    [
-        (
-            "https://ru.hexlet.io/teams",
-            "/var/tmp",
-            0,
-            f"{FIXTURES_PATH}/page-with-assets.html"
-        ),
-        (
-            "https://ru.hexlet.io",
-            "/var/tmp",
-            0,
-            f"{FIXTURES_PATH}/page-with-assets.html"
-        ),
-    ]
-)
-def test_cmd_parse_valid(url, output, expected_code, path):
-    with requests_mock.Mocker(real_http=True) as mock_request:
-        with open(path, 'r') as ctx:
-            mock_request.get(url, text=ctx.read())
-            command = ["poetry", "run", "page-loader", url, f"-o={output}"]
-            out, err, exitcode = capture(command)
-            print(out, err)
-            assert exitcode == expected_code, "Trouble while execute"
 
 
 @pytest.mark.parametrize(
