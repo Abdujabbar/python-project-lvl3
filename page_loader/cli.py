f1dad44b86cc786b83477ae92e2b95cb272f2138
Abdu <abdujabbar.mirkhalikov@gmail.com>
2022-06-01 14:35:48 +0500

    Modify cli

--- page_loader/cli.py
+++ page_loader/cli.py
@@ -2,11 +2,11 @@ import argparse
 import os
 
 
-def get_cmd_args(argv=[]):
+def get_cmd_args():
     parser = argparse.ArgumentParser(description='Page loader tool.')
     parser.add_argument('url', metavar='url')
 
     parser.add_argument('-o', '--output',
                         help='set path to store', default=os.getcwd())
 
-    return parser.parse_args(argv)
+    return parser.parse_args()
