883f6b1a55406805260e118bd89726d8bd7295f8
Abdu <abdujabbar.mirkhalikov@gmail.com>
2022-06-01 17:14:57 +0500

    Modify cli tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,5 +1,7 @@
+import requests_mock
 import pytest
 import subprocess
+from tests import FIXTURES_PATH
 
 
 def capture(command):
@@ -12,12 +14,38 @@ def capture(command):
 
 
 @pytest.mark.parametrize(
-    "url, output, expected_code",
+    "url, output, expected_code, path",
     [
         (
             "https://ru.hexlet.io/teams",
             "/var/tmp",
-            0
+            0,
+            f"{FIXTURES_PATH}/page-with-assets.html"
+        ),
+        (
+            "https://test.com",
+            "/var/tmp",
+            1,
+            f"{FIXTURES_PATH}/page-with-assets.html"
+        ),
+    ]
+)
+def test_cmd_parse_valid(url, output, expected_code, path):
+    with requests_mock.Mocker(real_http=True) as mock_request:
+        with open(path, 'r') as ctx:
+            mock_request.get(url, text=ctx.read())
+            command = ["poetry", "run", "page-loader", url, f"-o={output}"]
+            _, _, exitcode = capture(command)
+            assert exitcode == expected_code, "Trouble while execute"
+
+
+@pytest.mark.parametrize(
+    "url, output, expected_code",
+    [
+        (
+            "https://localhost:1233",
+            "/var/tmp",
+            1
         ),
         (
             "https://test.com",
@@ -26,7 +54,7 @@ def capture(command):
         ),
     ]
 )
-def test_cmd_parse_valid(url, output, expected_code):
+def test_cmd_exception(url, output, expected_code):
     command = ["poetry", "run", "page-loader", url, f"-o={output}"]
     _, _, exitcode = capture(command)
     assert exitcode == expected_code, "Trouble while execute"
